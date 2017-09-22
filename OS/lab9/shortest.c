c67f85a6eef07ebaa22ad4ffd133b26ef3a26b0a
Navneet Kishan <itsnavneetk@gmail.com>
2017-09-22 21:04:40 +0530

    Adding lab9 changes

--- OS/lab9/shortest.c
+++ OS/lab9/shortest.c
@@ -1,99 +1,136 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-// Define maximum number of vertices in the graph
-#define N 6
-struct Graph {
-	struct Node* head[N];
-};
-struct Node {
-	int dest, weight;
-	struct Node* next;
-};
-struct Edge {
-	int src, dest, weight;
-};
-struct Graph* createGraph(struct Edge edges[], int n)
+#include<stdio.h>
+#define MAX 10
+#define TEMP 0
+#define PERM 1
+#define infinity 9999
+struct node
 {
-	unsigned i;
-	// allocate memory for graph data structure
-	struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
-	// initialize head pointer for all vertices
-	for (i = 0; i < N; i++)
-		graph->head[i] = NULL;
-	// add edges to the directed graph one by one
-	for (i = 0; i < n; i++)
-	{
-		// get source and destination vertex
-		int src = edges[i].src;
-		int dest = edges[i].dest;
-        int weight = edges[i].weight;
-		// allocate new node of Adjacency List from src to dest
-		struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-		newNode->dest = dest;
-		newNode->weight = weight;
-		// point new node to current head
-		newNode->next = graph->head[src];
-		// point head pointer to new node
-		graph->head[src] = newNode;
-	}
-
-	return graph;
-}
-void printGraph(struct Graph* graph)
+          int predecessor;
+         int dist;
+          int status;
+};
+int adj[MAX][MAX];
+int n;
+void main()
 {
-	int i;
-	for (i = 0; i < N; i++)
-	{
-		struct Node* ptr = graph->head[i];
-		while (ptr != NULL)
-		{
-			printf("%d -> %d (%d)\t", i, ptr->dest, ptr->weight);
-			ptr = ptr->next;
-		}
-
-		printf("\n");
-	}
+          int i,j;
+          int source,dest;
+          int path[MAX];
+          int shortdist,count;
+          create_graph();
+          printf("The adjacency matrix is :\n");
+          display();
+          while(1)
+          {
+                  printf("\nEnter source node(0 to quit) : ");
+                   scanf("%d",&source);
+                   printf("\nEnter destination node(0 to quit) : ");
+                   scanf("%d",&dest);
+                   if(source==0 || dest==0)
+                             break;
+                   count = findpath(source,dest,path,&shortdist);
+                    if(shortdist!=0)
+                   {
+                             printf("\nShortest distance is : %d\n", shortdist);
+                             printf("\nShortest Path is : ");
+                             for(i=count;i>1;i--)
+                                      printf("%d->",path[i]);
+                             printf("%d",path[i]);
+                             printf("\n");
+                   }
+                   else
+                           printf("\nThere is no path from source to destination node\n");
+          }
 }
-int main(void)
+create_graph()
 {
-	struct Edge edges[] =
-	{
-		{ 0, 1, 6 }, { 1, 2, 7 }, { 2, 0, 5 }, { 2, 1, 4 },
-		{ 3, 2, 10 }, { 4, 5, 1 }, { 5, 4, 3 }
-	};
-	int n = sizeof(edges)/sizeof(edges[0]);
-	struct Graph *graph = createGraph(edges, n);
-	printGraph(graph);
-	return 0;
-	int v, w, length=0;
-	int path[6], reach[6];
-	printf("enter source and destination vertices \n");
-	scanf("%d",v);
-	scanf("%d",w);
-	printf(findpath(v, w, length, path, reach);
+          int i,max_edges,origin,destin,wt;
+          printf("Enter number of vertices : ");
+          scanf("%d",&n);
+          max_edges=n*(n-1);
+	   for(i=1;i<=max_edges;i++)
+          {
+                   printf("\nEnter edge %d(0 0 to quit) : ",i);
+                   scanf("%d %d",&origin,&destin);
+                  if((origin==0) && (destin==0))
+                            break;
+                 printf("\nEnter weight for this edge : ");
+                   scanf("%d",&wt);
+                   if( origin > n || destin > n || origin<=0 || destin<=0)
+                   {
+                             printf("\nInvalid edge!\n");
+                             i--;
+                   }
+                   else
+                             adj[origin][destin]=wt;
+          }
+}
+display()
+{
+          int i,j;
+          for(i=1;i<=n;i++)
+          {
+                   for(j=1;j<=n;j++)
+                            printf("%3d",adj[i][j]);
+                   printf("\n");
+          }
 }
 
-bool findpath(int v, int w,  int &length, int path[], int reach[]){
-	reach[v] = 1;
-	int u = Begin(v);
-	while(u){
-		if(!reach[u]){
-			length++;
-	path[length] = u;
-	if(u==v)
-		return true;
-	if(findpath(u, w, length, path, reach))
-		return true;
-		length--;
-		}
-	u = nextVertex(v);
-		}
-	return false;
+int findpath(int s,int d,int path[MAX],int *sdist)
+{
+          struct node state[MAX];
+          int i,min,count=0,current,newdist,u,v;
+          *sdist=0;
+          for(i=1;i<=n;i++)
+          {
+                   state[i].predecessor=0;
+                   state[i].dist = infinity;
+                   state[i].status = TEMP;
+          }
+          state[s].predecessor=0;
+          state[s].dist = 0;
+          state[s].status = PERM;
+          current=s;
+          while(current!=d)
+          {
+                   for(i=1;i<=n;i++)
+                   {
+                             if ( adj[current][i] > 0 && state[i].status == TEMP )
+                             {
+                                      newdist=state[current].dist + adj[current][i];
+                                      if( newdist < state[i].dist )
+                                      {
+                                                state[i].predecessor = current;
+                                               state[i].dist = newdist;
+                                      }
+                             }
+                   }
+                   min=infinity;
+                   current=0;
+                  for(i=1;i<=n;i++)
+                   {
+                             if(state[i].status == TEMP && state[i].dist < min)
+                             {
+                                     min = state[i].dist;
+                                      current=i;
+                             }
+                   }
+                   if(current==0)
+                            return 0;
+                   state[current].status=PERM;
+          }
+          while( current!=0 )
+          {
+                   count++;
+                  path[count]=current;
+                    current=state[current].predecessor;
+          }
+          for(i=count;i>1;i--)
+          {
+                   u=path[i];
+                   v=path[i-1];
+                 *sdist+= adj[u][v];
+
+         }
+          return (count);
 }
-
-
-
-
-
-
